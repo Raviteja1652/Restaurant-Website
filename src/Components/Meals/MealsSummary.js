587395e3127718845fa1a624d4f31cf2cf8de2fa
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-06 13:00:54 +0530

    Adding Restaurant Summary

---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import './MealsSummary.css';
+
+const MealsSummary = () => {
+    return(
+        <div className='summary'>
+            <h3>Delicious Food, Delivered To You</h3>
+            <p>Choose your favorite meal from our broad section of available meals and enjoy a Delicious lunch or Dinner at home.</p>
+            <p>All our meals are cooked with high quality ingredients, just-in-time and ofcourse by experienced Chefs.</p>
+        </div>
+    )
+};
+
+export default MealsSummary;
